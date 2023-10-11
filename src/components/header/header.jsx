5d1a29919351b0d4a3362028a14d98dda466c65e
FedericoHerreraa <fede.juan.herrera@gmail.com>
2023-10-11 19:03:06 -0300

    cambios en el header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,29 @@
 import React from "react"
+import './header.css'
 
 const Header = () => {
     return (
-        <h1>hola</h1>
+        <>
+            <nav className="navContainer">
+                <div className="navTitle">
+                    <p>Aromas PLC</p>
+                </div>
+                <div className="navInfo">
+                    <p>Productos</p>
+                    <p>Eventos</p>
+                    <p>Contacto</p>
+                </div>
+            </nav>
+            <div className="inicio">
+                <div className="titleContainer">
+                    <p>Bienvenidos</p>
+                    <h1>AROMAS PLC</h1>
+                </div>
+            </div>
+            <div className="verMas">
+                <p>Ver mas</p>
+            </div>
+        </>
     )
 }
 
