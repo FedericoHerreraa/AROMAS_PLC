f5a97a96c4b5c8de67f7261456289282b8edb385
FedericoHerreraa <fede.juan.herrera@gmail.com>
2023-11-28 17:52:28 -0300

    ..

--- src/components/eventos/eventos.jsx
+++ src/components/eventos/eventos.jsx
@@ -3,7 +3,7 @@ import stylesEvento from "./eventos.module.css"
 const Eventos = () => {
   return (
     <div className={stylesEvento.eventosContainer}>
-        <div>
+        <div className={stylesEvento.info}>
             <h2>¿No sabes qué dar como souvenir en tu evento?</h2>
             <p>Nosotros podemos asesorarte, accedé a nuestro catálogo!</p>
             <a target="blank" href="https://drive.google.com/file/d/12V6eW3LxEEB--8m4l4P-uVr1Ta8kSCuz/view?usp=drive_link">Catálogo</a>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,35 +5,35 @@ import { AiOutlineInstagram } from "react-icons/ai"
 
 const Footer = () => {
   return (
-    <div className={stylesFooter.footercontainer}> 
-        <footer>
-      <div className={stylesFooter.footerContent}>
-        <div className={stylesFooter.contactInfo}>
-          <h4>Contacto</h4>
-          <div>              
-            <p> <BiLogoGmail/> aromass.plc@gmail.com</p>
+    <div className={stylesFooter.footercontainer}>
+      <footer>
+        <div className={stylesFooter.footerContent}>
+          <div className={stylesFooter.contactInfo}>
+            <h4>Contacto</h4>
+            <div>
+              <p> <BiLogoGmail /> aromass.plc@gmail.com</p>
+            </div>
+            <p><PiMapPinFill /> Monte Castro, CABA</p>
           </div>
-          <p><PiMapPinFill/> Monte Castro, CABA</p>
-        </div>
-        <div className={stylesFooter.socialLinks}>
-          <h4>Redes Sociales</h4>
-          <ul>
-              <AiOutlineInstagram/>
+          <div className={stylesFooter.socialLinks}>
+            <h4>Redes Sociales</h4>
+            <ul>
+              <AiOutlineInstagram />
               <a href="#">Instagram</a>
-          </ul>
+            </ul>
+          </div>
+          <div className={stylesFooter.siteLinks}>
+            <h4>Enlaces del Sitio</h4>
+            <ul>
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Productos</a></li>
+            </ul>
+          </div>
         </div>
-        <div className={stylesFooter.siteLinks}>
-          <h4>Enlaces del Sitio</h4>
-          <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Productos</a></li>
-          </ul>
+        <div className={stylesFooter.copyright}>
+          <p>&copy; 2023 Aromas PLC. Todos los derechos reservados.</p>
         </div>
-      </div>
-      <div className={stylesFooter.copyright}>
-        <p>&copy; 2023 Aromas PLC. Todos los derechos reservados.</p>
-      </div>
-    </footer>
+      </footer>
     </div>
   )
 }
