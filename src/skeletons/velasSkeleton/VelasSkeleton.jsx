ab7242c4aef4e7c6670dea337d28ae54ff237d29
federicoherrera <fede.juan.herrera@gmail.com>
2024-05-08 13:51:25 -0300

    skeletons

--- src/skeletons/velasSkeleton/VelasSkeleton.jsx
+++ src/skeletons/velasSkeleton/VelasSkeleton.jsx
@@ -1,27 +1,26 @@
 import stylesVelas from "./VelasSkeleton.module.css";
 
 const VelasSkeleton = () => {
-    const skeletonItems = Array.from({ length: 4 }, (_, index) => (
-        <div key={index} className={stylesVelas.card}>
-            <div className={stylesVelas.contImg}>
-                <div className={stylesVelas.skeletonImage}></div>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px', marginTop: '20px' }}>
-                <div className={stylesVelas.skeletonTitle}></div>
-                <div className={stylesVelas.skeletonDescription}></div>
-                <div className={stylesVelas.skeletonButton}></div>
-            </div>
-        </div>
-    ));
+  // Genera un array de 2 elementos para mostrar 2 items en el skeleton
+  const skeletonItems = Array.from({ length: 2 }, (_, index) => (
+    <div key={index} className={stylesVelas.card}>
+      <div className={stylesVelas.skeletonImage}></div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '20px', marginTop: '20px' }}>
+        <div className={stylesVelas.skeletonTitle}></div>
+        <div className={stylesVelas.skeletonDescription}></div>
+        <div className={stylesVelas.skeletonButton}></div>
+      </div>
+    </div>
+  ));
 
-    return (
-        <div className={stylesVelas.container}>
-            <div className={stylesVelas.proximo}>
-                <h3>Velas Aromáticas</h3>
-            </div>
-            <div>{skeletonItems}</div>
-        </div>
-    );
+  return (
+    <div className={stylesVelas.container}>
+      <div className={stylesVelas.proximo}>
+        <h3 className={stylesVelas.title}>Velas aromáticas</h3>
+      </div>
+      <div>{skeletonItems}</div>
+    </div>
+  );
 };
 
 export default VelasSkeleton;
