6cb564f222c3d344dc657989737f62010e29facb
federicoherrera <fede.juan.herrera@gmail.com>
2024-05-08 13:52:51 -0300

    skeletons

--- src/components/productos/productos.jsx
+++ src/components/productos/productos.jsx
@@ -22,76 +22,75 @@ const Productos = () => {
   }, [getProducts, loading])
 
 
-  // function Arrow(props) {
-  //   // eslint-disable-next-line react/prop-types
-  //   const { className, onClick } = props;
-  //   return (
-  //     <div
-  //       className={className}
-  //       style={{ 
-  //         display: "block", 
-  //         fontSize: '40px', 
-  //         background: 'none',
-  //         backgroundColor: 'black', 
-  //         color: 'black',
-  //         borderRadius: '12px',
-  //         border: '1px solid grey'
-  //       }}
-  //       onClick={onClick}
-  //     />
-  //   );
-  // }
+  function Arrow(props) {
+    // eslint-disable-next-line react/prop-types
+    const { className, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ 
+          display: "block", 
+          fontSize: '40px', 
+          background: 'none',
+          backgroundColor: 'black', 
+          color: 'black',
+          borderRadius: '12px',
+          border: '1px solid grey'
+        }}
+        onClick={onClick}
+      />
+    );
+  }
   
-  // const [sliderSettings] = useState({
-  //   infinite: true,
-  //   className: stylesProducts.slider,
-  //   dots: true,
-  //   centerMode: true,
-  //   speed: 600,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 3,
-  //   arrows: true,
-  //   nextArrow: <Arrow/>,
-  //   prevArrow: <Arrow/>,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //         centerMode: false,
-  //         autoplay: true,
-  //         autoplaySpeed: 3000,
-  //       },
-  //     },
-  //   ],
+  const [sliderSettings] = useState({
+    infinite: true,
+    className: stylesProducts.slider,
+    dots: true,
+    centerMode: true,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    arrows: true,
+    nextArrow: <Arrow/>,
+    prevArrow: <Arrow/>,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          autoplay: true,
+          autoplaySpeed: 3000,
+        },
+      },
+    ],
 
-  // });
+  });
 
 
 
-  // if (loading) {
-  //   return <ProductosSkeleton/>
-  // }
+  if (loading) {
+    return <ProductosSkeleton/>
+  }
 
   return (
-    <ProductosSkeleton/>
-    // <div className={stylesProducts.container}>
-    //   <h1 className={stylesProducts.tituloProds}>Nuestros Productos</h1>
-    //   <Slider {...sliderSettings}>
-    //     {products.map((prod) => (
-    //       <Link key={prod._id} className={stylesProducts.detalle} to={`/detalle/${prod._id}`}>
-    //         <div className={stylesProducts.prod}>
-    //           <img src={prod.img} className={stylesProducts.img} alt="No se pudo cargar la imagen"/>
-    //           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-    //             <p className={stylesProducts.nombre}>{prod.nombre}</p>
-    //             <p>Precio: ${prod.precio}</p>
-    //           </div>
-    //         </div>
-    //       </Link>
-    //     ))}
-    //   </Slider>
-    // </div>
+    <div className={stylesProducts.container}>
+      <h1 className={stylesProducts.tituloProds}>Nuestros Productos</h1>
+      <Slider {...sliderSettings}>
+        {products.map((prod) => (
+          <Link key={prod._id} className={stylesProducts.detalle} to={`/detalle/${prod._id}`}>
+            <div className={stylesProducts.prod}>
+              <img src={prod.img} className={stylesProducts.img} alt="No se pudo cargar la imagen"/>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <p className={stylesProducts.nombre}>{prod.nombre}</p>
+                <p>Precio: ${prod.precio}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </Slider>
+    </div>
   );
 }
 
